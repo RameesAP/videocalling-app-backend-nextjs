11872f3fe3ad09e4dd56923837e27d8400d35e1d
Ramees <rameeswork@gmail.com>
2025-01-09 02:36:54 +0530

    ngrok env need to change every closeing and opeing time 2:11:40

--- generateUserToken/route.ts
+++ generateUserToken/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
 
-export async function POST(req:Request){
-    
+export async function POST(req: Request) {
+  const { userId, name, image, email } = await req.json();
 }
